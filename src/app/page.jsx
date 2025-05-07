d629f72985fb558d9d7e79266cf90fd6ce83abf1
Chea-MengLim <menglimchea@gmail.com>
2025-05-07 11:08:09 +0700

    completed scan qr code of ticket  using webcam

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,17 @@ import StripePaymentComponent from './components/StripePaymentComponent'
 import Onboard from './stripe-create-account/onboard'
 import CheckoutPage from './components/CheckoutPage'
 import EventDetail from './components/EventDetail'
+import TicketQRCode from './components/TicketQRCode'
+import QRScanner from './components/QRScanner'
 
 const HomePage = () => {
   return (
     <div>
       {/* <Onboard/> */}
       {/* <StripePaymentComponent/> */}
-      <EventDetail/>
+      {/* <EventDetail/> */}
+      {/* <TicketQRCode /> */}
+      <QRScanner />
     </div>
   )
 }
